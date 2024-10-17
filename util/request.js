d8707fe72c4454ed2e4c97a78a966e19195a009d
111inhistory <59101115+111inhistory@users.noreply.github.com>
2024-10-17 10:59:47 +0800

    Update request.js
    Correct iPhone UA and cookie

--- util/request.js
+++ util/request.js
@@ -47,9 +47,9 @@ const osMap = {
     channel: 'xiaomi',
   },
   iphone: {
-    os: 'iOS',
-    appver: '9.0.90',
-    osver: '16.2',
+    os: 'iPhone OS',
+    appver: '9.1.70',
+    osver: '18.0.1',
     channel: 'distribution',
   },
 }
@@ -67,7 +67,7 @@ const chooseUserAgent = (crypto, uaType = 'pc') => {
       pc: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36 Chrome/91.0.4472.164 NeteaseMusicDesktop/3.0.18.203152',
       android:
         'NeteaseMusic/9.1.65.240927161425(9001065);Dalvik/2.1.0 (Linux; U; Android 14; 23013RK75C Build/UKQ1.230804.001)',
-      iphone: 'NeteaseMusic 9.0.90/5038 (iPhone; iOS 16.2; zh_CN)',
+      iphone: 'NeteaseMusic 9.1.70/5410 (iPhone; iOS 18.0.1; zh_CN)',
     },
   }
   return userAgentMap[crypto][uaType] || ''
